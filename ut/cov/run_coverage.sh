861f6bcbd62f8629afc2d95d7ce8b2451b84e31c
piashishi <piashishi@qq.com>
2015-05-26 21:39:44 +0800

    fix source can not show

--- ut/cov/run_coverage.sh
+++ ut/cov/run_coverage.sh
@@ -5,6 +5,6 @@ make
 ./cache_ut
 mv *.gcda cov
 cd cov
-lcov --capture --directory ../ --output-file cache.info
+lcov --capture --directory ../  -b ../ --output-file cache.info
 genhtml cache.info --output-directory ./
 rm *.gcno *.gcda
