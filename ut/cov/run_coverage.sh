34757a33677ac98fb31ecce7de73298179011a97
mirhan <handachang@gmail.com>
2015-05-30 21:53:42 +0800

    convert format of run_coverage.sh

--- ut/cov/run_coverage.sh
+++ ut/cov/run_coverage.sh
@@ -6,5 +6,11 @@ make
 mv *.gcda cov
 cd cov
 lcov --capture --directory ../  -b ../ --output-file cache.info
+if [ $? -ne 0 ] 
+then 
+echo "LCOV error, Maybe need install LCOV firstly"
+exit -1
+fi
+
 genhtml cache.info --output-directory ./
 rm *.gcno *.gcda
