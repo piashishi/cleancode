42456f8e329d7acadc93c864f965a3eaf4462072
piashishi <piashishi@qq.com>
2015-06-11 20:41:29 +0800

    fix script bug

--- ut/cov/run_coverage.sh
+++ ut/cov/run_coverage.sh
@@ -3,7 +3,9 @@
 cd ..
 make ver=debug
 ./cache_ut
+sleep 1
 mv *.gcda cov
+mv *.gcno cov
 cd cov
 lcov --capture --directory ../  -b ../ --output-file cache.info
 if [ $? -ne 0 ] 
@@ -13,4 +15,3 @@ exit -1
 fi
 
 genhtml cache.info --output-directory ./
-rm *.gcno *.gcda
