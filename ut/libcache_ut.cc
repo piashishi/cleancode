2f8459f669bc6931dc3d0745fb093294eb18c944
piashishi <piashishi@qq.com>
2015-07-07 13:31:31 +0800

    change format

--- ut/libcache_ut.cc
+++ ut/libcache_ut.cc
@@ -4,40 +4,37 @@
 
 #include "UnitTest++.h"
 
-
 extern "C" {
 
 #include "libcache.h"
 #include "libcache_def.h"
 
-static	uint32_t test_key_to_int(const void* key)
-	{
-		uint32_t* value = (uint32_t*) key;
-		return *value;
-	}
+static uint32_t test_key_to_int(const void* key)
+{
+    uint32_t* value = (uint32_t*) key;
+    return *value;
+}
 
 static libcache_cmp_ret_t test_key_com(const void* key1, const void* key2)
-	{
-		uint32_t* a = (uint32_t*) key1;
-		uint32_t* b = (uint32_t*) key2;
+{
+    uint32_t* a = (uint32_t*) key1;
+    uint32_t* b = (uint32_t*) key2;
 
-		if (*a == *b) {
-			return LIBCACHE_EQU;
-		} else if (*a < *b) {
-			return LIBCACHE_SMALLER;
-		} else {
-			return LIBCACHE_BIGER;
-		}
-	}
-
+    if (*a == *b) {
+        return LIBCACHE_EQU;
+    } else if (*a < *b) {
+        return LIBCACHE_SMALLER;
+    } else {
+        return LIBCACHE_BIGER;
+    }
+}
 
 const libcache_scale_t g_max_entry_number = 100;
 
-typedef struct test_data_t
-{
+typedef struct test_data_t {
     void* key;
     void* entry;
-}test_data_t;
+} test_data_t;
 
 }
 
@@ -45,7 +42,14 @@ struct LibCacheFixture {
     void* g_cache;
     LibCacheFixture()
     {
-        g_cache = libcache_create(g_max_entry_number, sizeof(int), sizeof(int), malloc, free, NULL, test_key_com, test_key_to_int);
+        g_cache = libcache_create(g_max_entry_number,
+                                  sizeof(int),
+                                  sizeof(int),
+                                  malloc,
+                                  free,
+                                  NULL,
+                                  test_key_com,
+                                  test_key_to_int);
     }
     ~LibCacheFixture()
     {
@@ -57,7 +61,7 @@ TEST_FIXTURE(LibCacheFixture, TestAdd)
 {
     int key = 100;
     int entry = 1000;
-    int* value = (int*)libcache_add(g_cache, &key, &entry);
+    int* value = (int*) libcache_add(g_cache, &key, &entry);
     CHECK(*value == entry);
 
     //check add same key/entry
@@ -76,20 +80,19 @@ TEST_FIXTURE(LibCacheFixture, TestAdd)
 
     //check look up
     int entry4 = 0;
-    int* value4 = (int*)libcache_lookup(g_cache, &key3, &entry4);
+    int* value4 = (int*) libcache_lookup(g_cache, &key3, &entry4);
     CHECK(entry4 == 3000);
     CHECK(*value4 == 3000);
 
-    value4 = (int*)libcache_lookup(NULL, &key3, &entry4);
+    value4 = (int*) libcache_lookup(NULL, &key3, &entry4);
     CHECK(value4 == NULL);
 
-    value4 = (int*)libcache_lookup(g_cache, NULL, &entry4);
+    value4 = (int*) libcache_lookup(g_cache, NULL, &entry4);
     CHECK(value4 == NULL);
 
     //check look up
-    int* value5 = (int*)libcache_lookup(g_cache, &key3, NULL);
+    int* value5 = (int*) libcache_lookup(g_cache, &key3, NULL);
     CHECK(value5 == value3);
-
 
     ret = libcache_clean(g_cache);
     CHECK(ret == LIBCACHE_SUCCESS);
@@ -100,10 +103,7 @@ TEST_FIXTURE(LibCacheFixture, TestAdd)
     g_cache = libcache_create(10, sizeof(int), sizeof(int), NULL, NULL, NULL, test_key_com, test_key_to_int);
     CHECK(g_cache == NULL);
 
-
 }
-
-
 
 TEST_FIXTURE(LibCacheFixture, TestLookup)
 {
@@ -144,7 +144,6 @@ TEST_FIXTURE(LibCacheFixture, TestLookup)
     value2 = (int*) libcache_add(g_cache, &key2, &entry2);
     CHECK(value2 == NULL);
 
-
     libcache_ret_t ret = LIBCACHE_SUCCESS;
     for (i = 0; i <= g_max_entry_number; i++) {
         ret = libcache_unlock_entry(g_cache, entryList[i]);
@@ -176,7 +175,6 @@ TEST_FIXTURE(LibCacheFixture, TestDelete)
 
     libcache_scale_t count = libcache_get_entry_number(g_cache);
     CHECK_EQUAL(count, 3);
-
 
     libcache_ret_t ret = libcache_delete_by_key(NULL, &key);
     CHECK_EQUAL(ret, LIBCACHE_FAILURE);
@@ -253,18 +251,25 @@ TEST_FIXTURE(LibCacheFixture, TestSwap)
 
     const libcache_scale_t max_entry_number = 65535;
 
-    g_cache = libcache_create(max_entry_number, sizeof(int), sizeof(int), malloc, free, NULL, test_key_com, test_key_to_int);
+    g_cache = libcache_create(max_entry_number,
+                              sizeof(int),
+                              sizeof(int),
+                              malloc,
+                              free,
+                              NULL,
+                              test_key_com,
+                              test_key_to_int);
 
-	for (i = 0; i < max_entry_number * 10; i++) {
-		int* value4 = (int*) libcache_add(g_cache, &i, &i);
-		CHECK_EQUAL(*value4, i);
-	}
-	int count = libcache_get_entry_number(g_cache);
-	CHECK_EQUAL(count, max_entry_number+1);
+    for (i = 0; i < max_entry_number * 10; i++) {
+        int* value4 = (int*) libcache_add(g_cache, &i, &i);
+        CHECK_EQUAL(*value4, i);
+    }
+    int count = libcache_get_entry_number(g_cache);
+    CHECK_EQUAL(count, max_entry_number + 1);
 
-	for (i = 0; i <max_entry_number ; i++) {
-		int entry5 = 0;
-		int* value5 = (int*) libcache_lookup(g_cache, &i, &entry5);
-		CHECK(value5 == NULL);
-	}
+    for (i = 0; i < max_entry_number; i++) {
+        int entry5 = 0;
+        int* value5 = (int*) libcache_lookup(g_cache, &i, &entry5);
+        CHECK(value5 == NULL);
+    }
 }
